463bdba95ca0d0f8a21c12b21bfb05fef96dea9c
nuttyguy360 <smsovereign3@gmail.com>
2020-11-26 13:56:30 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/GraphingCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphingCalculator.dir/main.cpp.obj"
+  "GraphingCalculator.exe"
+  "GraphingCalculator.exe.manifest"
+  "GraphingCalculator.pdb"
+  "libGraphingCalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphingCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
